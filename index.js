257260202bfb31eb67e2ec247883a3cfead0a51d
Your Name <you@example.com>
2021-03-31 17:15:26 +0600

    add db user name

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const MongoClient = require('mongodb').MongoClient;
 require('dotenv').config()
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.x2r2l.mongodb.net/emaJohnStore?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.x2r2l.mongodb.net/${process.env.DB_Name}?retryWrites=true&w=majority`;
 
 const app = express()
 
