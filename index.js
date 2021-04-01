47d84ffcc7e4bbc8f8eb135eb8b2ef26457154cd
Your Name <you@example.com>
2021-04-01 11:35:02 +0600

    spece

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const app = express()
 
-app.use(bodyParser.json()); 
+app.use(bodyParser.json());
 app.use(cors());
 
 const port = 3001
