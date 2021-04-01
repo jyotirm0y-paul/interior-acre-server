78eb13331390427ccabaedf8deb544da29064c64
Your Name <you@example.com>
2021-04-01 11:46:05 +0600

    log

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ require('dotenv').config()
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.x2r2l.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
-const app = express()
+const app = express() 
 
 app.use(bodyParser.json());
 app.use(cors());
